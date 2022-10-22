6eb7bb7934dc6b50d367c83baa07f587e8e69e03
Leva <parimalleva@gmail.com>
2022-10-22 17:56:12 +0300

    add code in word-field.css

---
+++ modules/word-field.js
@@ -0,0 +1,26 @@
+export default class WordField {
+    constructor() {
+        this.words = [{
+            text: 'javascript'
+        }, {
+            text: 'swift'
+        }, {
+            text: 'java'
+        }, {
+            text: 'python'
+        }, {
+            text: 'kotlin'
+        }, {
+            text: 'pascal'
+        }, {
+            text: 'ruby'
+        }, {
+            text: 'groovy'
+        }, {
+            text: 'cobol'
+        }, {
+            text: 'scala'
+        }];
+    }
+}
+
